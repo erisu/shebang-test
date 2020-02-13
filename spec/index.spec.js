4533c6935cc6d18873d23e3948164f71deac2e9e
Erisu <ellis.bryan@gmail.com>
2020-02-13 10:20:12 +0900

    has found the problem here

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,10 +1,12 @@
+const rewire = require('rewire');
+
 describe('shebang', () => {
   beforeEach(() => {
     spyOn(console, 'log');
   });
 
   it('shows that shebang causes parsing error', () => {
-    require('../index');
+    rewire('../index');
     expect(console.log).toHaveBeenCalledWith('Success');
   });
 });
